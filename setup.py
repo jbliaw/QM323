f9494c106eb7fa6db47dd87f6ac2d6f02d601ad8
jbliaw <65575099+jbliaw@users.noreply.github.com>
2020-10-06 19:45:16 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='not_DML',
+    version='0.1.0',
+    author='Jojo Liaw',
+    packages=find_packages(),
+    license='MIT License',
+)
+
+# In the same style as: https://github.com/rll/deepul
